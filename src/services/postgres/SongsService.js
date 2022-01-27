65ed4237251cf0fa963aadb6fe3de92fd6ee5e5b
Deri Kurniawan <deri.netuchi@gmail.com>
2022-01-27 23:05:42 +0700

    change camelCase to snake_case

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -59,7 +59,7 @@ class SongsService {
   }) {
     const updatedAt = new Date().toISOString();
     const query = {
-      text: `UPDATE ${this._table} SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, albumId = $6, updated_at = $7 WHERE id = $8 RETURNING id`,
+      text: `UPDATE ${this._table} SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6, updated_at = $7 WHERE id = $8 RETURNING id`,
       values: [title, year, genre, performer, duration, albumId, updatedAt, id],
     };
 
